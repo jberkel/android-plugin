204a9a142da5363cd0183fd3e29f3df54aeb0c36
Mark Harrah <dmharrah@gmail.com>
2009-07-06 23:08:19 -0400

    Updated README and made it ready to publish to scala-tools

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,6 +2,9 @@ import sbt._
 
 class AndroidPlugin(info: ProjectInfo) extends PluginProject(info)
 {
+	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+	Credentials(Path.fromFile(System.getProperty("user.home")) / ".ivy2" / ".credentials", log)
+	
 	val proguard = "net.sf.proguard" % "proguard" % "4.3"
 	val ant = "org.apache.ant" % "ant" % "1.7.1" intransitive()
 	val regexp = "org.apache.ant" % "ant-apache-regexp" % "1.7.1" intransitive()
