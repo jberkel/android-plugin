c1c03c33fa814ca99ea8e61ed36eae5ce9f0c07c
Jan Berkel <jan.berkel@gmail.com>
2009-11-11 19:00:29 +0100

    comment

--- src/main/scala/AndroidProject.scala
+++ src/main/scala/AndroidProject.scala
@@ -199,8 +199,8 @@ abstract class AndroidProject(info: ProjectInfo) extends DefaultProject(info) {
   }
   
   
-  // these dependencies are already included in the  
-  // they won't get included in build
+  // these dependencies are already included in the Android SDK 
+  // set the configuration to "provided" so they won't get included in the package
   val http_core = "org.apache.httpcomponents" % "httpcore" % "4.0.1" % "provided"
   val http_client = "org.apache.httpcomponents" % "httpclient" % "4.0" % "provided"
   val logging = "commons-logging" % "commons-logging"  % "1.1.1" % "provided"
