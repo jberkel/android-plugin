df72718c45e26e68be9772784bb1fbc29bde2bc7
Jan Berkel <jan.berkel@gmail.com>
2009-11-11 18:43:21 +0100

    use proguard 4.4

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,7 +5,7 @@ class AndroidPlugin(info: ProjectInfo) extends PluginProject(info)
 	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 	Credentials(Path.fromFile(System.getProperty("user.home")) / ".ivy2" / ".credentials", log)
 	
-	val proguard = "net.sf.proguard" % "proguard" % "4.3"
+	val proguard = "net.sf.proguard" % "proguard" % "4.4"
 	val ant = "org.apache.ant" % "ant" % "1.7.1" intransitive()
 	val regexp = "org.apache.ant" % "ant-apache-regexp" % "1.7.1" intransitive()
 	val antLauncher = "org.apache.ant" % "ant-launcher" % "1.7.1"
