caee028bd30c6ef4eb32e7d56ee3c86f08c040b9
Jan Berkel <jan.berkel@gmail.com>
2009-12-07 10:43:12 +0100

    rename runTest to testEmulator/testDevice

--- src/main/scala/AndroidTestProject.scala
+++ src/main/scala/AndroidTestProject.scala
@@ -2,7 +2,9 @@ import sbt._
 
 abstract class AndroidTestProject(info: ProjectInfo) extends AndroidProject(info) {
   override def proguardInJars = Path.emptyPathFinder
-  
-  lazy val runTest = runTestAction
-  def runTestAction = adbTask(true, "shell am instrument -w "+manifestPackage+"/android.test.InstrumentationTestRunner") describedAs("runs instrumentation tests")        
+
+  lazy val testEmulator = instrumentationTestAction(true) describedAs("runs tests in emulator")
+  lazy val testDevice = instrumentationTestAction(false) describedAs("runs tests on device")
+
+  def instrumentationTestAction(emulator:Boolean) = adbTask(emulator, "shell am instrument -w "+manifestPackage+"/android.test.InstrumentationTestRunner") describedAs("runs instrumentation tests")        
 }
