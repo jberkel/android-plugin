89ca3085f8b9de61c6ac4c7209a4824972cd57eb
Paul Butcher <paul@paulbutcher.com>
2011-04-30 14:27:47 +0100

    Stop start-emulator and start-device from trying to start the tests app

--- src/main/scala/AndroidTestProject.scala
+++ src/main/scala/AndroidTestProject.scala
@@ -3,6 +3,10 @@ import sbt._
 abstract class AndroidTestProject(info: ProjectInfo) extends AndroidProject(info) {
   override def proguardInJars = Path.emptyPathFinder
 
+  // Normally it makes no sense to start a test application, so make these actions no-ops.
+  override def startEmulatorAction = task { None }
+  override def startDeviceAction = task { None }
+
   lazy val testEmulator = instrumentationTestAction(true) describedAs("runs tests in emulator") dependsOn reinstallEmulator
   lazy val testDevice = instrumentationTestAction(false) describedAs("runs tests on device") dependsOn reinstallDevice
 
