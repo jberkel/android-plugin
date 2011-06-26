ce340c1d6e39fe1274d01cc8891b21c6ea3572a1
Philip Cali <philip.cali@gmail.com>
2011-06-26 16:34:11 -0500

    Moved sources to temp, undoing project definitions

--- project/plugins/Plugins.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-  val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
-  val sxr_publish = "net.databinder" % "sxr-publish" % "0.1.9"
-}
