a822cb17c48a3a9069ad3ed6bf8e041a11d78bd6
Philip Cali <philip.cali@gmail.com>
2011-06-29 16:47:20 -0500

    Moved Android Setting and Task keys into own file.

--- src/main/scala/BaseAndroidProject.scala
+++ src/main/scala/BaseAndroidProject.scala
@@ -1,66 +1,9 @@
 import sbt._
 import Keys._
 
+import AndroidKeys._
+
 object BaseAndroidProject extends Plugin {
-  val Android = config("android")
-
-  /** Default Settings */
-  val aptName = SettingKey[String]("apt-name")
-  val dbName = SettingKey[String]("db-name")
-  val idlName = SettingKey[String]("idl-name")
-  val dxName = SettingKey[String]("dx-name")
-  val manifestName = SettingKey[String]("manifest-name")
-  val jarName = SettingKey[String]("jar-name")
-  val mapsJarName = SettingKey[String]("maps-jar-name")
-  val assetsDirectoryName = SettingKey[String]("assets-dir-name")
-  val resDirectoryName = SettingKey[String]("res-dir-name")
-  val classesMinJarName = SettingKey[String]("classes-min-jar-name")
-  val classesDexName = SettingKey[String]("classes-dex-name")
-  val resourcesApkName = SettingKey[String]("resources-apk-name")
-  val dxJavaOpts = SettingKey[String]("dx-java-opts")
-  val manifestSchema = SettingKey[String]("manifest-schema")
-  val envs = SettingKey[Seq[String]]("envs")
-
-  // Determined on OS
-  val packageApkName = SettingKey[String]("package-apk-name")
-  val osDxName = SettingKey[String]("os-dx-name")
-
-  // Override this setting
-  val platformName = SettingKey[String]("platform-name", "Targetted android platform")
-
-  // Determined Settings 
-  val manifestPackage = SettingKey[String]("manifest-package")
-  val minSdkVersion = SettingKey[Option[Int]]("min-sdk-version")
-  val maxSdkVersion = SettingKey[Option[Int]]("max-sdk-version")
-  val apiLevel = SettingKey[Int]("api-level")
-
-  val sdkPath = SettingKey[File]("sdk-path")
-  val toolsPath = SettingKey[File]("tools-path")
-  val dbPath = SettingKey[File]("db-path")
-  val platformPath = SettingKey[File]("platform-path")
-  val platformToolsPath = SettingKey[File]("platform-tools-path")
-  val aptPath = SettingKey[File]("apt-path")
-  val idlPath = SettingKey[File]("idl-path")
-  val dxPath = SettingKey[File]("dx-path")
-
-  val manifestPath = SettingKey[File]("manifest-path")
-  val jarPath = SettingKey[File]("jar-path")
-  val nativeLibrariesPath = SettingKey[File]("natives-lib-path")
-  val addonsPath = SettingKey[File]("addons-path")
-  val mapsJarPath = SettingKey[File]("maps-jar-path")
-  val mainAssetsPath = SettingKey[File]("main-asset-path")
-  val mainResPath = SettingKey[File]("main-res-path")
-  val managedJavaPath = SettingKey[File]("managed-java-path")
-  val classesMinJarPath = SettingKey[File]("classes-min-jar-path")
-  val classesDexPath = SettingKey[File]("classes-dex-path")
-  val resourcesApkPath = SettingKey[File]("resources-apk-path")
-  val packageApkPath = SettingKey[File]("package-apk-path")
-  val skipProguard = SettingKey[Boolean]("skip-proguard")
-
-  val addonsJarPath = SettingKey[Seq[File]]("addons-jar-path")
-
-  /** Tasks */
-  val aptGenerate = TaskKey[Unit]("apt-generate")
   private def aptGenerateTask: Project.Initialize[Task[Unit]] = 
     (manifestPackage, aptPath, manifestPath, mainResPath, jarPath, managedJavaPath) map {
     (mPackage, aPath, mPath, resPath, jPath, javaPath) => Process (<x>
@@ -73,7 +16,6 @@ object BaseAndroidProject extends Plugin {
     </x>) !
   }
 
-  val aidlGenerate = TaskKey[Unit]("aidl-generate")
   private def aidlGenerateTask: Project.Initialize[Task[Unit]] = 
     (sourceDirectories, idlPath, managedJavaPath, javaSource) map {
     (sDirs, idPath, javaPath, jSource) =>
@@ -94,11 +36,7 @@ object BaseAndroidProject extends Plugin {
       }.get
   }
 
-  // Helpers
-  def adbTask(emulator: Boolean, action: => String) = (dbPath) map { dPath => <x>
-    {dPath.absolutePath} {if (emulator) "-e" else "-d"} {action}
-  </x>}
-
+  // Internal Helpers
   private def determineAndroidSdkPath(es: Seq[String]) = {
     val paths = for ( e <- es; p = System.getenv(e); if p != null) yield p
     if (paths.isEmpty) None else Some(Path(paths.head).asFile)
