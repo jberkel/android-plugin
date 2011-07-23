f63c442e12803bd8624d7e4299db4be5c078f3ac
Philip Cali <philip.cali@gmail.com>
2011-07-23 00:57:02 -0500

    Adding a module just for sdk paths.

---
+++ src/main/scala/AndroidPath.scala
@@ -0,0 +1,28 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+import AndroidHelpers._
+
+object AndroidPath {
+
+  lazy val settings: Seq[Setting[_]] = inConfig(Android) { 
+    AndroidDefaults.settings ++ Seq (
+    osDxName <<= (dxName) (_ + osBatchSuffix),
+
+    toolsPath <<= (sdkPath) (_ / "tools"),
+    dbPath <<= (platformToolsPath, adbName) (_ / _),
+    platformPath <<= (sdkPath, platformName) (_ / "platforms" / _),
+    platformToolsPath <<= (sdkPath) (_ / "platform-tools"),
+    aaptPath <<= (platformToolsPath, aaptName) (_ / _),
+    idlPath <<= (platformToolsPath, aidlName) (_ / _),
+    dxPath <<= (platformToolsPath, osDxName) (_ / _),
+    jarPath <<= (platformPath, jarName) (_ / _),
+
+    sdkPath <<= (envs) { es => 
+      determineAndroidSdkPath(es).getOrElse(error(
+        "Android SDK not found. You might need to set %s".format(es.mkString(" or "))
+      ))
+    }
+  ) }
+}
