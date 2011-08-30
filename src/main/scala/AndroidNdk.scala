9a66101969013f87ce9bf2516cfc786f5d6a7074
Martin Kneissl <m.kneissl@dasgip.de>
2011-08-30 10:26:08 +0200

    Removed unnecessary keys + minor cleanup for NDK builds.

--- src/main/scala/AndroidNdk.scala
+++ src/main/scala/AndroidNdk.scala
@@ -1,8 +1,7 @@
 import sbt._
 
 import Keys._
-import AndroidKeys._
-import AndroidHelpers._
+import AndroidKeys.Android
 import AndroidNdkKeys._
 
 import java.io.File
@@ -23,8 +22,6 @@ object AndroidNdk {
   val DefaultJniDirectoryName = "jni"
   /** The default directory name for compiled native objects. */
   val DefaultObjDirectoryName = "obj"
-  /** The default directory name for compiled native libraries. */
-  val DefaultLibsDirectoryName = "libs"
   /** The list of environment variables to check for the NDK. */
   val DefaultEnvs = List("ANDROID_NDK_HOME", "ANDROID_NDK_ROOT")
 
@@ -33,12 +30,10 @@ object AndroidNdk {
     ndkBuildName := DefaultNdkBuildName,
     jniDirectoryName := DefaultJniDirectoryName,
     objDirectoryName := DefaultObjDirectoryName,
-    libsDirectoryName := DefaultLibsDirectoryName,
     ndkEnvs := DefaultEnvs
     ))
 
   // ndk-related paths
-    
   lazy val pathSettings: Seq[Setting[_]] = inConfig(Android) (Seq (
     jniSourcePath <<= (sourceDirectory, jniDirectoryName) (_ / _),
     nativeOutputPath <<= (jniSourcePath) (_.getParentFile),
@@ -57,7 +52,8 @@ object AndroidNdk {
   ))
     
 
-  private def ndkBuildTask(targets: String*) = (ndkBuildPath, nativeOutputPath, nativeLibrariesPath) map {(ndkBuildPath, obj, libs) =>
+  private def ndkBuildTask(targets: String*) = 
+    (ndkBuildPath, nativeOutputPath) map { (ndkBuildPath, obj) =>
       val exitValue = Process(ndkBuildPath.absolutePath :: "-C" :: obj.absolutePath :: targets.toList) !
       
       if(exitValue != 0) error("ndk-build failed with nonzero exit code (" + exitValue + ")")
@@ -71,8 +67,6 @@ object AndroidNdk {
     (compile in Compile) <<= (ndkBuild in Android, compile in Compile) map { (ndkBuild, compile) => ndkBuild ; compile }
   )) ++ Seq (
     cleanFiles <+= (nativeObjectPath in Android).identity, 
-    //cleanFiles <+= (nativeLibrariesPath in Android).identity
     clean <<= (clean, ndkClean in Android) map { (clean, ndkClean) => ndkClean ; clean  }  
   )
 }
-  /* FIXME: Make compile depend on ndkBuild. */
--- src/main/scala/AndroidNdkKeys.scala
+++ src/main/scala/AndroidNdkKeys.scala
@@ -10,13 +10,12 @@ object AndroidNdkKeys {
   val ndkBuildName = SettingKey[String]("ndk-build-name", "Name for the 'ndk-build' tool")
   val jniDirectoryName = SettingKey[String]("jni-directory-name", "Directory name for native sources.")
   val objDirectoryName =  SettingKey[String]("obj", "Directory name for compiled native objects.")
-  val libsDirectoryName = SettingKey[String]("libs", "Directory name for compiled native libraries.")
   val ndkEnvs = SettingKey[Seq[String]]("ndk-envs", "List of environment variables to check for the NDK.")
 
-  val jniSourcePath = SettingKey[File]("jni-source-path")
+  val jniSourcePath = SettingKey[File]("jni-source-path", "Path to native sources. (with Android.mk)")
   val nativeOutputPath = SettingKey[File]("native-output-path")
   val nativeObjectPath = SettingKey[File]("native-object-path")
-  val ndkBuildPath = SettingKey[File]("ndk-build-path")
+  val ndkBuildPath = SettingKey[File]("ndk-build-path", "Path to the 'ndk-build' tool")
   
   val ndkBuild = TaskKey[Unit]("ndk-build", "Compile native C/C++ sources.")
   val ndkClean = TaskKey[Unit]("ndk-clean", "Clean resources built from native C/C++ sources.")
