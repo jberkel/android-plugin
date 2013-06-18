de915fb34b7f6430adf766ceeb031f645496253d
François-Xavier Thomas <fx.thomas@gmail.com>
2013-06-18 17:08:41 +0200

    [fix] "empty.reduceLeft" error when no buildTools

--- src/main/scala/AndroidPath.scala
+++ src/main/scala/AndroidPath.scala
@@ -15,19 +15,30 @@ object AndroidPath {
     if (!buildToolsPath.exists) None
 
     // Else, sort the installed versions and take the most recent
-    else Some(buildToolsPath.listFiles.map(_.name).reduceLeft((s1, s2) => {
+    else {
+      // List the versions
+      val files = buildToolsPath.listFiles
 
-      // Convert the version numbers to arrays of integers
-      val v1 = s1 split '.' map (_.toInt)
-      val v2 = s2 split '.' map (_.toInt)
+      // If the directory is empty, return None
+      if (files.isEmpty) None
 
-      // Compare them
-      // (Will crash if the version numbers don't contain 3 digits)
-      if((v1(0) > v2(0)) ||
-        (v1(0) == v2(0) && v1(1) > v2(1)) ||
-        (v1(0) == v2(0) && v1(1) == v2(1) && v1(2) > v2(1)))
-        s1 else s2
-    }))
+      // Else, find the latest version
+      else Some(buildToolsPath.listFiles.map(_.name).reduceLeft((s1, s2) => {
+
+        // Convert the version numbers to arrays of integers
+        val v1 = s1 split '.' map (_.toInt)
+        val v2 = s2 split '.' map (_.toInt)
+
+        // TODO: Make that better. Will crash if the version numbers don't
+        //       have 3 digits (like 17.0.0)...
+
+        // Compare them
+        if((v1(0) > v2(0)) ||
+          (v1(0) == v2(0) && v1(1) > v2(1)) ||
+          (v1(0) == v2(0) && v1(1) == v2(1) && v1(2) > v2(1)))
+          s1 else s2
+      }))
+    }
   }
 
   lazy val settings: Seq[Setting[_]] = {
